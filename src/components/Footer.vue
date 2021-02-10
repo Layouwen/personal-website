a05dc6f0a3c184ff9ce982af4c9a8edea87a7883
layouwen <layouwen@gmail.com>
2021-02-10 09:44:21 +0800

    add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer_wrapper"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Footer extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.footer_wrapper {
+  height: 100px;
+  background: #fff;
+}
+</style>
