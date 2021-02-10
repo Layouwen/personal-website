43089355454830fe4e541d661897656977dfde02
layouwen <layouwen@gmail.com>
2021-02-10 09:48:54 +0800

    add route

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,16 +7,22 @@ Vue.use(VueRouter)
 const routes: Array<RouteConfig> = [
   {
     path: '/',
+    redirect: '/index'
+  },
+  {
+    path: '/index',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '*',
+    name: 'notFound',
+    component: () => import('../views/notFound.vue')
   }
 ]
 
