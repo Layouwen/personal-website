ff4f1adbc896b81fe978f609cb6d0c4b8a8f52e8
layouwen <layouwen@gmail.com>
2021-02-10 09:48:27 +0800

    introduce Header and Footer components into App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Nav />
+    <router-view />
+    <Footer />
   </div>
 </template>
+
+<script lang="ts">
+import Footer from '@/components/Footer.vue'
+import Nav from '@/components/Header.vue'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    Footer,
+    Nav
+  }
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import "~@/assets/styles/reset.scss";
