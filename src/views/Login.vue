05cc9a68402f93044c70476646711e4c7894d8ff
layouwen <layouwen@gmail.com>
2021-02-10 11:00:16 +0800

    complete the Login and SignUp styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,18 @@
 <template>
-  <Layout class="login-wrapper">
-    我是登录页
+  <Layout class="login_wrapper">
+    <form @submit.prevent="onSubmit" class="login">
+      <h2>登录</h2>
+      <label>
+        账号：<input v-model="loginInfo.account" />
+      </label>
+      <label>
+        密码：<input v-model="loginInfo.password" type="password" />
+      </label>
+      <div class="buttons">
+        <button type="submit" class="login_btn">登录</button>
+        <button type="button" class="sign_up_btn">注册</button>
+      </div>
+    </form>
   </Layout>
 </template>
 
@@ -9,9 +21,66 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class Login extends Vue {
+  loginInfo = {
+    account: '',
+    password: ''
+  }
+
+  onSubmit = () => {
+    console.log(this.loginInfo)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.login_wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 594px;
+    height: 580px;
+    background: #fff;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.08);
+    border-radius: 14px;
+    > h2 {
+      margin: 36px 0 64px;
+      font-size: 44px;
+    }
+    > label {
+      font-size: 20px;
+      &:not(:first-child) {
+        margin-top: 30px;
+      }
+      > input {
+        width: 250px;
+        height: 36px;
+        padding: 0 10px;
+        background: #F5F5F5;
+        border: 1px solid #E2E2E2;
+        border-radius: 18px;
+      }
+    }
+    > .buttons {
+      margin-top: 60px;
+      > button {
+        width: 178px;
+        height: 36px;
+        font-size: 20px;
+        color: #fff;
+        border-radius: 18px;
+      }
+      > .login_btn {
+        margin-right: 12px;
+        background: #82B4FF;
+      }
+      > .sign_up_btn {
+        background: #FF5858;
+      }
+    }
+  }
+}
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,17 +1,86 @@
 <template>
-  <Layout class="sign_up_wrapper"></Layout>
+  <Layout class="sign_up_wrapper">
+    <form @submit.prevent="onSubmit" class="sign_up">
+      <h2>注册</h2>
+      <label>
+        账号：<input v-model="signUpInfo.account" />
+      </label>
+      <label>
+        密码：<input v-model="signUpInfo.password" type="password" />
+      </label>
+      <label>
+        确认密码：<input v-model="signUpInfo.confirmPassword" type="password" />
+      </label>
+      <div class="buttons">
+        <button type="submit" class="sign_up_btn">注册</button>
+      </div>
+    </form>
+  </Layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
-export default class SignUp extends Vue {
+export default class Login extends Vue {
+  signUpInfo = {
+    account: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  onSubmit = () => {
+    console.log(this.signUpInfo)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .sign_up_wrapper {
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .sign_up {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 594px;
+    height: 580px;
+    background: #fff;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.08);
+    border-radius: 14px;
+    > h2 {
+      margin: 36px 0 64px;
+      font-size: 44px;
+    }
+    > label {
+      display: flex;
+      justify-content: flex-end;
+      width: 350px;
+      font-size: 20px;
+      &:not(:first-child) {
+        margin-top: 30px;
+      }
+      > input {
+        width: 250px;
+        height: 36px;
+        padding: 0 10px;
+        background: #F5F5F5;
+        border: 1px solid #E2E2E2;
+        border-radius: 18px;
+      }
+    }
+    > .buttons {
+      margin-top: 60px;
+      > .sign_up_btn {
+        width: 236px;
+        height: 36px;
+        font-size: 20px;
+        color: #fff;
+        background: #82B4FF;
+        border-radius: 18px;
+      }
+    }
+  }
 }
 </style>
