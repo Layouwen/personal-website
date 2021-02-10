a31bab9ef63d0f17df640e66cc4527f67f0fbc2d
layouwen <layouwen@gmail.com>
2021-02-10 12:37:35 +0800

    import all icons

---
+++ src/components/Icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="`#`+name"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+// import all icons
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/))
+} catch (error) {
+  console.error(error)
+}
+
+@Component({})
+export default class Icon extends Vue {
+  @Prop() name !: string
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
