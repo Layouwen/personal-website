1a03754fcc17efc4cce115d9c85348b6e7c0492c
layouwen <layouwen@gmail.com>
2021-02-10 10:12:37 +0800

    add Layout component

---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="layout">
+    <slot />
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Layout extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  min-height: calc(100vh - 80px - 100px);
+  background: #C3EDFE;
+}
+</style>
