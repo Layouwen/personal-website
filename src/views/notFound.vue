e6e3c2352f3b11e9477626cfe74418c4090cad7b
layouwen <layouwen@gmail.com>
2021-02-10 09:43:09 +0800

    add notFound page

---
+++ src/views/notFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>404</div>
+</template>
+
+<script lang="ts">
+import Nav from '@/components/Header.vue'
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+  components: { Nav }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
